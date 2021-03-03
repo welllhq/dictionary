b56e54cebeabe762b5ea7e2ef6fd401abd3e55b1
973040629@qq.com <973040629@qq.com>
2021-03-03 14:46:40 +0800

    针对原来的网页变动更改了部分代码

--- dictionary.py
+++ dictionary.py
@@ -7,7 +7,7 @@ import sys
 #作为模块
 def dictionary(target):
     #模拟谷歌访问url
-    url ='http://www.iciba.com/'
+    url ='http://www.iciba.com/word?w='
     f_url = url + target
     header = {'User-Agent':' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
     req = urllib.request.Request(url = f_url,headers = header)
@@ -17,9 +17,10 @@ def dictionary(target):
     #使用beautifulsoup 解析html
     soup = BeautifulSoup(html,'lxml')
     res=[]
-    res = soup.select('ul>.clearfix>p>span')
+    res = soup.select('ul .Mean_part__1RA2V>li>i')+soup.select('ul .Mean_part__1RA2V>li>div>span')
+    
 
-    #去标签
+    #去标签以及拼写错误检查
     f_res=[]
     if res:
         for i in res:
@@ -30,7 +31,7 @@ def dictionary(target):
     #根据解析内容返回翻译
     return f_res
 
-##单独运行,若作为模块则注释此处
+##单独运行。若作为模块则注释此处
 if __name__ == '__main__':
    easygui.msgbox(msg = '注意，本程序必须联网运行',title = 
                   'created ------by Wells',ok_button ='确定')
